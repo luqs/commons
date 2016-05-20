401edb84839f4d5c5e56e45566bdc602d5cabc5f
lanwm <12345678>
2016-05-20 13:59:48 +0800

    common-redis：添加依赖修改代码

--- commons-redis/src/main/java/com/cuize/commons/dao/order/domain/JedisUtil.java
+++ commons-redis/src/main/java/com/cuize/commons/dao/order/domain/JedisUtil.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.controller.RedisTestController;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
--- commons-utils/src/main/java/com/cuize/commons/utils/JedisUtil.java
+++ commons-utils/src/main/java/com/cuize/commons/utils/JedisUtil.java
@@ -37,11 +37,11 @@ public class JedisUtil {
 			JedisPoolConfig config = new JedisPoolConfig();
 			// 控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
 			// 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-			config.setMaxTotal(500);
+			/*config.setMaxTotal(500);*/
 			// 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
 			config.setMaxIdle(5);
 			// 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
-			config.setMaxWaitMillis(1000*100);
+			/*config.setMaxWaitMillis(1000*100);*/
 			// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
 			config.setTestOnBorrow(true);
 			// pool = new JedisPool(config, "192.168.0.121", 6379, 100000);
@@ -1926,8 +1926,18 @@ public class JedisUtil {
 	 * @param redis
 	 */
 	public static void returnResource(JedisPool pool, Jedis jedis) {
-		if (jedis != null) {
-			jedis.close();
+		if (pool != null ) {
+			if (jedis != null) {
+				pool.returnResource(jedis);
+			}
+		} else {
+			if (jedis != null) {
+				if (jedis.isConnected()) {
+					jedis.quit();
+					jedis.disconnect();
+				}
+			}
 		}
+		
 	}
 }
